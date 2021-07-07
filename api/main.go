67eeaa615d3dbeb1f5c7c8cde89ac4d0614e4d7c
Camille Arsac <camille.arsac26@gmail.com>
2021-07-07 12:45:21 +0200

    fix

--- api/main.go
+++ api/main.go
@@ -5,22 +5,16 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	)
+)
 
 func main() {
 	router := initRouter()
-	if err != nil {
-		log.Fatal("es client Failed", err)
-	}
-	log.Println(esClient)
 	log.Println("Running on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func initRouter () *mux.Router{
+func initRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/search", controllers.Search).Methods("POST")
 	return r
 }
-
-
