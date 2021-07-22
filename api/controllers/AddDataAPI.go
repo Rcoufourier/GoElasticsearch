b2e4ce67614c5c8701b5c2dce74e78282361d73c
James Bissick <bissickj@gmail.com>
2021-07-22 20:29:31 +0200

    feat: - reading and printing data from Googlebooks API

--- api/controllers/AddDataAPI.go
+++ api/controllers/AddDataAPI.go
@@ -29,12 +29,15 @@ type TmpBook struct {
 	} `json:"items"`
 } */
 
-type APIBooks struct {
-	Title         string  `njson:"items.#.volumeInfo.title"`
-	Author        string  `njson:"items.#.volumeInfo.authors.0"`
-	Abstract      string  `njson:"items.#.volumeInfo.description"`
+type APILibrary struct {
+	Items []string `njson:"items"`
 }
 
+type APIBooks struct {
+	Title         string  `njson:"volumeInfo.title"`
+	Author        string  `njson:"volumeInfo.authors.0"`
+	Abstract      string  `njson:"volumeInfo.description"`
+}
 
 func AddDataAPI(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json;charset=UTF-8")
@@ -45,34 +48,41 @@ func AddDataAPI(w http.ResponseWriter, r *http.Request) {
 	//req := "https://www.googleapis.com/books/v1/volumes?maxResults=40" + keyword + apiKey
 	// https://www.googleapis.com/books/v1/volumes?q=test&maxResults=40&key=AIzaSyCT_Pt_MCIoLBd4BHVWfA1lyd7R1YOWnPw
 
-	res, err := http.Get("https://www.googleapis.com/books/v1/volumes?q=test&maxResults=10&key=AIzaSyCT_Pt_MCIoLBd4BHVWfA1lyd7R1YOWnPw")
+	res, err := http.Get("https://www.googleapis.com/books/v1/volumes?q=test&maxResults=5&key=AIzaSyCT_Pt_MCIoLBd4BHVWfA1lyd7R1YOWnPw")
 	if err != nil {
 		fmt.Println("Cannot get request!")
 	}
 
 	responseData, err := ioutil.ReadAll(res.Body)
 	// fmt.Println(string(responseData) + "\n---------------------------------------------\n")
-
-	var tmpbook APIBooks
+	/* var tmpbook APIBooks
 	errorMessage := njson.Unmarshal([]byte(string(responseData)), &tmpbook)
 	if err != nil {
 		panic(errorMessage)
-	}
-
-	fmt.Printf("%+v\n\n", tmpbook)
-	/*
-	responseData, err := ioutil.ReadAll(res.Body)
+	} */
+	// fmt.Printf("%+v\n\n", tmpbook)
 	//fmt.Println(string(responseData))
-	//resData := map[string][][]interface{}{}
-	errormessage := json.Unmarshal([]byte(string(responseData)), &tmpbook)
+
+	var resData APILibrary
+	errormessage := njson.Unmarshal([]byte(string(responseData)), &resData)
 	if err != nil {
 		fmt.Println(errormessage)
 	}
 
-	book := APIBooks {
-		Title : tmpbook.items[0].volumeInfo.title,
+	var dataTab []APIBooks
+
+	for _, data := range resData.Items {
+		//fmt.Println("------------------------------------")
+		var finalData APIBooks
+		errormessage = njson.Unmarshal([]byte(string(data)), &finalData)
+		if errormessage != nil {
+			fmt.Println(errormessage)
+		}
+		dataTab = append(dataTab, finalData)
 	}
 
-	fmt.Println(book)
-	fmt.Println(tmpbook) */
+	fmt.Println(dataTab[2].Author)
+	fmt.Println("------------------------------------")
+
+
 }
